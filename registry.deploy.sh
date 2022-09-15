d179aed95f002777d5d0caff85fd3ac2a8873aa2
Jonathan Chen <jonchen228@gmail.com>
2022-09-15 12:53:09 -0400

    standardize deploy/remove shell scripts

--- registry.deploy.sh
+++ registry.deploy.sh
@@ -3,8 +3,8 @@
 WD=$(dirname $0)
 SCRIPTS_DIR=${WD}/../../scripts
 export ENV_DIR=${WD}/../../../compose/conf/swarm-env-config
+export ENV_FILE=${ENV_DIR}/swarm.env
 export COMPOSE_FILE=${WD}/registry-swarm.yml
 export STACK_NAME=registry-swarm
-export ENV_FILE=${ENV_DIR}/swarm.env
 
 ${SCRIPTS_DIR}/stack.deploy.sh
--- registry.remove.sh
+++ registry.remove.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-SCRIPTS_DIR=../../scripts
-STACK_NAME=registry-swarm ${SCRIPTS_DIR}/stack.remove.sh
+WD=$(dirname $0)
+SCRIPTS_DIR=${WD}/../../scripts
+export STACK_NAME=registry-swarm
+
+${SCRIPTS_DIR}/stack.remove.sh
